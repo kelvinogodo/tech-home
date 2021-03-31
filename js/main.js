66bc0305d997ff3316e4ba8c396cecd992347608
ogodo kelvin <kelvinchukwuebuka385@gmail.com>
2021-03-31 01:05:35 -0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function showMenu(){
+    document.getElementById('menu-list').style.width='300px';
+   }
+   function closeMenu(){
+       document.getElementById('menu-list').style.width='0px';
+      }
